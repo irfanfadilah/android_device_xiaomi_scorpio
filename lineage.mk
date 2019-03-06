0398d5c11108eeffcd125a1f0c80f343a788a319
Irfan Fadilah <hello@irfanfadilah.com>
2019-03-07 03:35:12 +0700

    Update Build Information

--- lineage.mk
+++ lineage.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="scorpio" \
     PRODUCT_NAME="scorpio" \
-    PRIVATE_BUILD_DESC="scorpio-user 8.0.0 OPR1.170623.032 V9.6.1.0.OADMIFD release-keys"
+    PRIVATE_BUILD_DESC="scorpio-user 8.0.0 OPR1.170623.032 V10.2.1.0.OADMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:8.0.0/OPR1.170623.032/V9.6.1.0.OADMIFD:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:8.0.0/OPR1.170623.032/V10.2.1.0.OADMIXM:user/release-keys"
